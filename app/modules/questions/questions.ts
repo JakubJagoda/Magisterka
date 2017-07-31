56507ebafd726298c84e16114f8c70d1c7ba5d6f
Kuba Jagoda <kuba.jagoda+github@gmail.com>
2017-07-31 22:55:11 +0200

    Choosing questions that has been chosen least times

--- app/modules/questions/questions.ts
+++ app/modules/questions/questions.ts
@@ -4,6 +4,10 @@ import {IPlainQuestion, Question} from './question';
 
 export {Question};
 
+interface IInternalPlainQuestion extends IPlainQuestion {
+    timesSelected: number;
+}
+
 interface IGetDataResponseEntry {
     content: string;
     content_id: string;
@@ -40,9 +44,9 @@ interface ISendAnswersPhrasesPayloadEntry {
 }
 
 const LOCAL_STORAGE_KEY = 'tgame.questions';
-let questionsStorage = new Map<number, Question[]>();
+let questionsStorage = new Map<number, IInternalPlainQuestion[]>();
 
-function convertResponseToQuestions(response:IGetDataResponseEntry[]):IPlainQuestion[] {
+function convertResponseToPlainQuestions(response:IGetDataResponseEntry[]):IPlainQuestion[] {
     return response.map(responseEntry => {
         const {word, definition} = transformContentStringToDefinition(responseEntry.content);
 
@@ -75,33 +79,49 @@ export function getQuestion(level: number):Question {
         return new Question(); // well, this should never end here
     }
 
-    return questions[Math.floor(Math.random() * questions.length)];
+    const minTimesSelected = questions.reduce((currentMin, question) => {
+        return Math.min(currentMin, question.timesSelected);
+    }, Infinity);
+    const highestChanceQuestions = questions.filter(question => question.timesSelected === minTimesSelected);
+    const selectedQuestion = highestChanceQuestions[Math.floor(Math.random() * highestChanceQuestions.length)];
+
+    if (!selectedQuestion) {
+        return new Question(); // well, this should never end here
+    }
+
+    selectedQuestion.timesSelected++;
+    saveQuestionsStorage();
+
+    return Question.fromPlainQuestion(selectedQuestion);
 }
 
-async function fetchQuestions():Promise<Question[]> {
-    const questionsInStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) as IPlainQuestion[];
+async function fetchQuestions():Promise<IInternalPlainQuestion[]> {
+    const questionsInStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) as IInternalPlainQuestion[];
     if (questionsInStorage) {
-        return questionsInStorage.map(plainQuestion => Question.fromPlainQuestion(plainQuestion));
+        return questionsInStorage;
     }
 
     return Api.getData(userStore.getUserData().userID, 100).then((response:IGetDataResponseEntry[]) => {
-        const mappedResponse = convertResponseToQuestions(response);
+        const mappedResponse = convertResponseToPlainQuestions(response).map(plainQuestion => {
+            return Object.assign({}, plainQuestion, {timesSelected: 0});
+        });
+
         localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(mappedResponse));
-        return mappedResponse.map(plainQuestion => Question.fromPlainQuestion(plainQuestion));
+        return mappedResponse;
     });
 }
 
-function mapQuestionsToQuestionStorage(questions: Question[]):Map<number, Question[]> {
-    return questions.reduce((reduced, question) => {
-        const difficulty = question.getDifficulty();
+function mapQuestionsToQuestionStorage(questions: IInternalPlainQuestion[]):Map<number, IInternalPlainQuestion[]> {
+    return questions.reduce((reduced, internalQuestion) => {
+        const difficulty = internalQuestion.difficulty;
 
         if (!reduced.has(difficulty)) {
             reduced.set(difficulty, []);
         }
 
-        reduced.set(difficulty, [...reduced.get(difficulty), question]);
+        reduced.set(difficulty, [...reduced.get(difficulty), internalQuestion]);
         return reduced;
-    }, new Map<number, Question[]>());
+    }, new Map<number, IInternalPlainQuestion[]>());
 }
 
 export async function loadInitialQuestionSet():Promise<void> {
@@ -123,4 +143,12 @@ function transformContentStringToDefinition(content: string) {
         word,
         definition
     };
+}
+
+function saveQuestionsStorage() {
+    const flattened = ([...questionsStorage.values()] as IInternalPlainQuestion[][]).reduce((reduced, questionStorageEntry) => {
+        return [...reduced, ...questionStorageEntry];
+    }, []);
+
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(flattened));
 }
